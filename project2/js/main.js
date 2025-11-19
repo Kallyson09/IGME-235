24680b6d2fbe84570d4d49c32d49e4f1eef5a527
Kallyson09 <allysonl.kuo@outlook.com>
2025-11-19 02:18:52 -0500

    working filters!! + fix backround

--- project2/js/main.js
+++ project2/js/main.js
@@ -24,7 +24,7 @@ function searchButtonClicked() {
 
     document.querySelector("#status").innerHTML = "<b>Searching for '" + displayTerm + "'</b>";
 
-    console.log(url);
+    // console.log(url);
 
     getData(url);
 }
@@ -55,15 +55,30 @@ function dataLoaded(e) {
 
     let results = obj.data;
 
-    //Sort if chose option
-    if (document.querySelector("#sortLimit").options[1].selected)
-    {
+    // Sort Z-A if option chosen, otherwise keep sorted
+    if (document.querySelector("#sortLimit").options[1].selected) {
         results = results.reverse();
     }
 
-    // Limit results based on 1. filters and 2. results
+    //if filter is selected, set which it will be
+    let filterChosen;
+        
+    // Identify which filter has been chosen
+    if (!document.querySelector("#filterLimit").options[0].selected) {
+        for (let i = 0; i < document.querySelector("#filterLimit").options.length; i++) {
+            if (document.querySelector("#filterLimit").options[i].selected) {
+                filterChosen = document.querySelector("#filterLimit").options[i].value;
+                break;
+            }
+        }
+        
+        // Loop through array and if the property != null/empty, add 
+        console.log("filter chosen: " + filterChosen);
+        console.log(Object.getOwnPropertyDescriptor(results[0], filterChosen).value);
+        let descriptor = Object.getOwnPropertyDescriptor(results[0], filterChosen).value;
+        results = results.filter(obj => { return (Object.getOwnPropertyDescriptor(obj, filterChosen).value.length > 0)});
+    }
 
-    // let resultsFiltered = results.filter(obj => { return obj.year <= 1970 });
 
     // Limit the results shown
     let limitAmount;
@@ -75,15 +90,22 @@ function dataLoaded(e) {
         }
     }
 
+    // Display results 
     let bigString = "";
 
+    // If limit is less than amount of results, only show the limit
     if (limitAmount < results.length) {
         for (let i = 0; i < limitAmount; i++) {
             let result = results[i];
 
             let line = `<div class = 'result'>`;
-            line += `<img src = '${result.imageUrl}' title = '${result.id}'/>`;
+            line += `<img src = '${result.imageUrl}' title = '${result.name}'/>`;
             line += `<span><p>${result.name}</p>`;
+            line += `<p>Films: ${result.films}</p>`;
+            line += `<p>Short Films: ${result.shortFilms}</p>`;
+            line += `<p>TV Shows: ${result.tvShows}</p>`;
+            line += `<p>Video Games: ${result.videoGames}</p>`;
+            line += `<p>Park Attractions: ${result.parkAttractions}</p>`;
             line += `<a target = '_blank' href='${result.imageUrl}'>View Image</a></span>`;
             line += `</div>`;
 
@@ -91,15 +113,20 @@ function dataLoaded(e) {
         }
 
         document.querySelector("#status").innerHTML = "<p>Showing " + limitAmount + " results for '" + displayTerm + "'</i></p>";
-
     }
     else {
+        // Limit is greater than amount of results, just show all results
         for (let i = 0; i < results.length; i++) {
             let result = results[i];
 
             let line = `<div class = 'result'>`;
-            line += `<img src = '${result.imageUrl}' title = '${result.id}'/>`;
+            line += `<img src = '${result.imageUrl}' title = '${result.name}'/>`;
             line += `<span><p>${result.name}</p>`;
+            line += `<p>Films: ${result.films}</p>`;
+            line += `<p>Short Films: ${result.shortFilms}</p>`;
+            line += `<p>TV Shows: ${result.tvShows}</p>`;
+            line += `<p>Video Games: ${result.videoGames}</p>`;
+            line += `<p>Park Attractions: ${result.parkAttractions}</p>`;
             line += `<a target = '_blank' href='${result.imageUrl}'>View Image</a></span>`;
             line += `</div>`;
 
@@ -113,6 +140,7 @@ function dataLoaded(e) {
 
     document.querySelector("#content").innerHTML = bigString;
 }
+
 
 function dataError(e) {
     console.log("An error occurred");
